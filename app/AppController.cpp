50185d4e23df6662aa49242edb11fa0bdc7f7dde
Nathan Jacques Edmond Tabet <nathan.tabet@epfl.ch>
2026-06-15 16:16:50 +0200

    cleaner shadow behind text

--- app/AppController.cpp
+++ app/AppController.cpp
@@ -750,9 +750,9 @@ void AppController::onARFrame(const cv::Mat &frame,
         QString angleMsg = tr("Angle : %1 (> 175 requis)").arg(dbgAngleDeg, 0, 'f', 1);
         std::string angleText = angleMsg.toStdString();
 
-        // Draw with shadow for better visibility, using bold font
+        // Draw with subtle shadow for better visibility, using bold font
         const cv::Scalar shadow(0, 0, 0);
-        cv::putText(out, angleText, {22, 72}, cv::FONT_HERSHEY_DUPLEX, 1.5, shadow, 6, cv::LINE_AA);
+        cv::putText(out, angleText, {21, 71}, cv::FONT_HERSHEY_DUPLEX, 1.5, shadow, 2, cv::LINE_AA);
         cv::putText(out, angleText, {20, 70}, cv::FONT_HERSHEY_DUPLEX, 1.5, color,  3, cv::LINE_AA);
     }
 
